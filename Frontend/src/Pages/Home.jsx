856125f37846d3b3c36598635907f332b944d742
diwakar-web <diwakarrajeshnagar@gmail.com>
2026-02-12 16:09:23 +0530

    4th commit

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -3,8 +3,14 @@ import './Home.css';
 import Aurora from "../Components/Backgrounds/Aurora"
 import Header from "../Components/FixedComponents/Header"
 import CardSwap, { Card } from '../Components/FixedComponents/CardSwap';
-
+import SplitText from '../Components/FixedComponents/SplitText';
+import CurvedLoop from '../Components/FixedComponents/CurvedLoop';
+import ScrollReveal from '../Components/FixedComponents/ScrollReveal';
+import ScrollVelocity from '../Components/FixedComponents/ScrollVelocity';
 export default function Home() {
+    const handleAnimationComplete = () => {
+        console.log('All letters have animated!');
+    };
     return (
         <div className="home-container page">
             <Aurora
@@ -23,23 +29,30 @@ export default function Home() {
                                 AI-POWERED FAMILY HEALTH & CARE PLATFORM
                             </p>
                             <h1 className='Heading'>
-                                One Platform for
+                                <SplitText
+                                    text="One Platform for
                                 Smarter Everyday
-                                Health Management
+                                Health Management"
+                                    className="text-2xl font-semibold text-center"
+                                    delay={50}
+                                    duration={1.25}
+                                    ease="power3.out"
+                                    splitType="chars"
+                                    from={{ opacity: 0, y: 40 }}
+                                    to={{ opacity: 1, y: 0 }}
+                                    threshold={0.1}
+                                    rootMargin="-100px"
+                                    textAlign="right"
+                                    onLetterAnimationComplete={handleAnimationComplete}
+                                    showCallback
+                                />
                             </h1>
                         </div>
                         <div className='Buttons'>
                             <button>Get Started</button>
                             <button>Explore Features</button>
-
-
-
                         </div>
-
                     </div>
-
-
-
                     <div className='sec2 sec'>
                         <CardSwap
                             cardDistance={60}
@@ -47,36 +60,63 @@ export default function Home() {
                             delay={5000}
                             pauseOnHover={false}>
                             <Card>
-                                <h3>💉 Smart Vaccination Tracker</h3>
+                                <h3 style={{ fontSize: "2em" }}>💉 Smart Vaccination Tracker</h3>
                                 <p>Stay ahead of every immunization milestone with ARTIKA’s intelligent vaccine tracking system. Based on age-specific schedules, it automatically identifies upcoming, due, or missed vaccines and sends timely reminders so nothing slips through. Protect your child with confidence and clarity.
                                     With downloadable vaccination records and simple status indicators, managing preventive care becomes effortless and organized.</p>
                             </Card>
                             <Card>
-                                <h3>📊 Intelligent Growth Monitoring</h3>
-                                <p>Monitor your child’s development with real-time insights powered by height, weight, BMI, and age-based growth comparisons. ARTIKA analyzes entered data and provides easy-to-understand feedback using healthy growth ranges. 
+                                <h3 style={{ fontSize: "2em" }}>📊 Intelligent Growth Monitoring</h3>
+                                <p>Monitor your child’s development with real-time insights powered by height, weight, BMI, and age-based growth comparisons. ARTIKA analyzes entered data and provides easy-to-understand feedback using healthy growth ranges.
                                     Track growth velocity, identify trends over time, and stay informed with non-diagnostic yet meaningful indicators designed to support informed parenting.</p>
                             </Card>
                             <Card>
-                                <h3>🧠 MediBot – AI Health Assistant</h3>
-                                <p>Get instant guidance for everyday health queries through MediBot, your smart AI-powered assistant. Whether it’s symptoms, wellness tips, or general health information, receive quick and structured responses anytime you need them. 
+                                <h3 style={{ fontSize: "2em" }}>🧠 MediBot – AI Health Assistant</h3>
+                                <p>Get instant guidance for everyday health queries through MediBot, your smart AI-powered assistant. Whether it’s symptoms, wellness tips, or general health information, receive quick and structured responses anytime you need them.
                                     Designed for accessibility and simplicity, MediBot helps reduce confusion while encouraging informed decision-making.</p>
                             </Card>
                             <Card>
-                                <h3>🗂️ Secure Health Locker</h3>
-                                <p>Keep all medical records organized in one encrypted digital vault. Upload prescriptions, lab reports, and insurance documents securely and access them whenever required. 
+                                <h3 style={{ fontSize: "2em" }}>🗂️ Secure Health Locker</h3>
+                                <p>Keep all medical records organized in one encrypted digital vault. Upload prescriptions, lab reports, and insurance documents securely and access them whenever required.
                                     With structured categorization and easy retrieval, ARTIKA ensures your important health data is never scattered or misplaced.</p>
                             </Card>
                             <Card>
-                                <h3>🚨 Emergency Mode</h3>
-                                <p>Be prepared when every second counts. ARTIKA’s Emergency Mode provides instant access to critical support features and nearby medical assistance. 
+                                <h3 style={{ fontSize: "2em" }}>🚨 Emergency Mode</h3>
+                                <p>Be prepared when every second counts. ARTIKA’s Emergency Mode provides instant access to critical support features and nearby medical assistance.
                                     Quick-response tools and accessible health information help you stay calm and act fast during urgent situations.</p>
                             </Card>
                         </CardSwap>
                     </div>
 
                 </div>
+                <div className='Numbers_section'>
+                    <CurvedLoop
+                        marqueeText="ARTIKA.life ✦ ARTIKA.life ✦ ARTIKA.life ✦ ARTIKA.life ✦ ARTIKA.life ✦"
+                        speed={2}
+                        curveAmount={200}
+                        direction="right"
+                        interactive
+                        className="custom-text-style"
+                    />
+                </div>
+                <div className='numbers'>
+                    <ScrollReveal
+                        baseOpacity={0.5}
+                        enableBlur
+                        baseRotation={3}
+                        blurStrength={5}>
+                        12300000+ families manage doctors, medicines, child care, and emergencies using multiple disconnected tools, leading to 26,00,000+ missed vaccinations and 1,80,00,000+ missed medicine doses every year.
+                        With 5,40,00,000+ smartphone users and 80,00,000+ daily health searches, ARTIKA unifies care into one intelligent platform for family health, prevention, and emergency readiness.
+                    </ScrollReveal>
+                </div>
+                <div className='ribbon'>
+                    <ScrollVelocity
+                        texts={['ARTIKA.life', 'What We Offer!']}
+                        velocity={100}
+                        className="custom-scroll-text"
+                    />
+                </div>
+            
             </div>
-
         </div>
     );
 }
